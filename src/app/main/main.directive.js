83baad69b2c16577b919798f639cbdfba8ff1da5
Sanford Guerrero <sanford.guerrero@dayzim.com>
2016-08-24 10:10:58 -0400

    First commit
    Made changes to the color scheme and added branding elements to the Yoh
    portal.

---
+++ src/app/main/main.directive.js
@@ -0,0 +1,48 @@
+class MainController {
+    constructor(SharedData) {
+        'ngInject';
+
+        this.SharedData = SharedData;
+        this.SharedData.isCollapsed = true;
+        this.SharedData.CareerResource = false;
+        this.SharedData.Diversity = false;
+        this.SharedData.About = false;
+    }
+
+    closeFilters() {
+        var $portalCanvas = document.querySelector('.portal-canvas');
+        var $mask = document.querySelector('#mask');
+
+        if ($portalCanvas) {
+            $portalCanvas.classList.remove('show-nav');
+        }
+
+        if ($mask) {
+            $mask.classList.remove('active');
+        }
+    }
+    closeCollapse() {
+        console.log(this.SharedData.isCollapsed);
+        this.SharedData.isCollapsed = !this.SharedData.isCollapsed;
+    }
+}
+
+class Main {
+    constructor() {
+        'ngInject';
+
+        let directive = {
+            restrict: 'E',
+            templateUrl: 'app/main/main.html',
+            scope: false,
+            controller: MainController,
+            controllerAs: 'main',
+            bindToController: true,
+            replace: true
+        };
+
+        return directive;
+    }
+}
+
+export default Main;
